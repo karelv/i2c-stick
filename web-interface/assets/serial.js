444ffa51c118dd3e52f40af5f5fb2d578640f2e3
karelv <karel.vanroye@gmail.com>
2023-11-21 23:45:17 -0800

    add again custom-console logging

--- web-interface/assets/serial.js
+++ web-interface/assets/serial.js
@@ -16,6 +16,9 @@
  * permissions and limitations under the License.
  */
 'use strict';
+
+var my_counter = 0;
+
 export var SerialPolyfillProtocol;
 (function (SerialPolyfillProtocol) {
     SerialPolyfillProtocol[SerialPolyfillProtocol["UsbCdcAcm"] = 0] = "UsbCdcAcm";
@@ -110,7 +113,11 @@ class UsbEndpointUnderlyingSource {
                 chunkSize = this.endpoint_.packetSize;
             }
             try {
+                let elem = document.querySelector('#console');
+                my_counter = my_counter + 1;
+                elem.innerHTML = elem.innerHTML + "<hr>START["+my_counter.toString()+"]: chunkSize: "+chunkSize.toString()+" -- this.endpoint_.endpointNumber: "+this.endpoint_.endpointNumber.toString()+" <br>";
                 const result = await this.device_.transferIn(this.endpoint_.endpointNumber, chunkSize);
+                elem.innerHTML = elem.innerHTML + "RESULT["+my_counter.toString()+"]: "+result.status+"<br>";
                 if (result.status != 'ok') {
                     controller.error(`USB error: ${result.status}`);
                     this.onError_();
@@ -118,6 +125,7 @@ class UsbEndpointUnderlyingSource {
                 if ((_a = result.data) === null || _a === void 0 ? void 0 : _a.buffer) {
                     const chunk = new Uint8Array(result.data.buffer, result.data.byteOffset, result.data.byteLength);
                     controller.enqueue(chunk);
+                    elem.innerHTML = elem.innerHTML + "DATA["+my_counter.toString()+"]: byteOffset: "+result.data.byteOffset.toString()+" | byteLength: "+result.data.byteLength.toString()+" <hr>";
                 }
             }
             catch (error) {
