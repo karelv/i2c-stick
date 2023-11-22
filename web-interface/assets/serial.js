6ececa425ed124d0ed5fc002de793cb27563b2a7
karelv <karel.vanroye@gmail.com>
2023-11-21 23:41:06 -0800

    back to builded serial.js

--- web-interface/assets/serial.js
+++ web-interface/assets/serial.js
@@ -16,9 +16,6 @@
  * permissions and limitations under the License.
  */
 'use strict';
-
-var my_counter = 0;
-
 export var SerialPolyfillProtocol;
 (function (SerialPolyfillProtocol) {
     SerialPolyfillProtocol[SerialPolyfillProtocol["UsbCdcAcm"] = 0] = "UsbCdcAcm";
@@ -113,23 +110,13 @@ class UsbEndpointUnderlyingSource {
                 chunkSize = this.endpoint_.packetSize;
             }
             try {
-                let elem = document.querySelector('#console');
-                my_counter = my_counter + 1;
-                elem.innerHTML = elem.innerHTML + "<hr>START["+my_counter.toString()+"]: chunkSize: "+chunkSize.toString()+" -- this.endpoint_.endpointNumber: "+this.endpoint_.endpointNumber.toString()+" <br>";
                 const result = await this.device_.transferIn(this.endpoint_.endpointNumber, chunkSize);
-                elem.innerHTML = elem.innerHTML + "RESULT["+my_counter.toString()+"]: "+result.status+"<br>";
                 if (result.status != 'ok') {
                     controller.error(`USB error: ${result.status}`);
                     this.onError_();
                 }
                 if ((_a = result.data) === null || _a === void 0 ? void 0 : _a.buffer) {
                     const chunk = new Uint8Array(result.data.buffer, result.data.byteOffset, result.data.byteLength);
-//                    let str = '';
-//                    let i;
-//                    for (i in chunk){
-//                        str+=String.fromCharCode(chunk[i]);
-//                    }
-                    elem.innerHTML = elem.innerHTML + "DATA["+my_counter.toString()+"]: result.data.byteOffset: "+result.data.byteOffset.toString()+" | result.data.byteLength: "+result.data.byteLength.toString()+"<hr>";
                     controller.enqueue(chunk);
                 }
             }
